59bf54edc9594da9f5216885a0df4cbc205e9a30
ovidiu-bazu <ovidiu.bazu@gmail.com>
2020-03-04 13:36:57 +0200

    stage-0

--- server/read-all-lessons.route.ts
+++ server/read-all-lessons.route.ts
@@ -4,6 +4,8 @@ import {db} from "./database";
 
 export function readAllLessons(req, res) {
 
-    res.status(200).json({lessons:db.readAllLessons()});
+  //setTimeout(() => {
+    res.status(200).json({lessons: db.readAllLessons()});
+  //},60000);
 
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,14 +10,18 @@ import {SwPush, SwUpdate} from "@angular/service-worker";
 export class AppComponent  implements OnInit {
 
 
-    constructor() {
+    constructor(private swUpdate: SwUpdate) {
 
     }
 
     ngOnInit() {
-
-
-
+      if (this.swUpdate.isEnabled) {
+        this.swUpdate.available.subscribe(() => {
+          if (confirm('New version available. Load New Version?')) {
+            window.location.reload();
+          }
+        });
+      }
     }
 
 }
